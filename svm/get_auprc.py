f5367a2aea5f274348f3c9bb4843cf94aed2656c
anna shcherbina <annashcherbina@gmail.com>
2020-02-10 16:16:15 -0800

    SVM benchmarking parser code

---
+++ svm/get_auprc.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.metrics import average_precision_score
+prefix="/srv/scratch/annashch/5_cell_lines_bias_correction/svm"
+import sys
+task=sys.argv[1]
+fold=sys.argv[2]
+labels=pd.read_csv(prefix+"/"+task+"/"+"svm_predictions_svmtrainset_genometestset"+"/"+"labels."+fold+".sorted.bed",header=None,sep='\t')
+preds=pd.read_csv(prefix+"/"+task+"/"+"svm_predictions_svmtrainset_genometestset"+"/"+"genomewidepredictions."+task+"."+fold+".sorted.bed",header=None,sep='\t')
+merged=pd.concat([labels,preds],axis=1).dropna()[3]
+merged.columns=['labels','preds']
+cur_auprc=average_precision_score(merged['labels'],merged['preds'])
+total=merged.shape[0]
+pos=sum(merged['labels'])
+neg=total-pos
+print(task+'\t'+fold+'\t'+str(cur_auprc)+'\t'+str(pos)+'\t'+str(neg))
