761c6eda9e3c65e4806df49a13649ab70f861e8a
anna shcherbina <annashcherbina@gmail.com>
2020-08-30 00:44:35 -0700

    SVM interpretation for canonical cell lines

---
+++ svm/interpet/sbatch_explain.sh
@@ -0,0 +1,7 @@
+for cell_line in K562 HEPG2
+do
+    for split in `seq 0 4`
+    do
+	sbatch -J $cell_line.$split -o logs_explain/$cell_line.$split.o -e logs_explain/$cell_line.$split.e -t 1-0 -p akundaje,euan,owners explain.sh 
+    done
+done
