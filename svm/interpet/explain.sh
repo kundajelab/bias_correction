4e0f0fea7875779d2fc96acfee6fd6f1c4e534d6
Anna Shcherbina <annashcherbina@gmail.com>
2020-08-30 11:19:04 -0700

    fix typo

--- svm/interpet/explain.sh
+++ svm/interpet/explain.sh
@@ -3,5 +3,5 @@ task=$1
 split=$2
 prefix=/oak/stanford/groups/akundaje/projects/enzymatic_bias_correction/svm/
 model=/oak/stanford/groups/akundaje/projects/enzymatic_bias_correction/svm/models/$task\_dnase/$task.$split.model.txt
-gkmexplain  $prefix/fasta/$split.DNASE.svm.10kb.to.interpret.fa $model  $prefix/pred/gkmexplain.$task.$split.dnase.txt
+gkmexplain  $prefix/fasta/$task.DNASE.svm.10kb.to.interpret.fa $model  $prefix/pred/gkmexplain.$task.$split.dnase.txt
 
