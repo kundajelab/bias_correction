3f2b7ae8af7d9f49075d3ba9ad49802454892103
Anna Shcherbina <annashcherbina@gmail.com>
2020-08-30 11:13:51 -0700

    corrected sbatch command

---
+++ svm/interpet/explain.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+task=$1
+split=$2
+prefix=/oak/stanford/groups/akundaje/projects/enzymatic_bias_correction/svm/
+model=/oak/stanford/groups/akundaje/projects/enzymatic_bias_correction/svm/models/$task\_dnase/$task.$split.model.txt
+gkmexplain  $prefix/fasta/$split.DNASE.svm.10kb.to.interpret.fa $model  $prefix/pred/gkmexplain.$task.$split.dnase.txt
+
--- svm/interpet/sbatch_explain.sh
+++ svm/interpet/sbatch_explain.sh
@@ -2,6 +2,6 @@ for cell_line in K562 HEPG2
 do
     for split in `seq 0 4`
     do
-	sbatch -J $cell_line.$split -o logs_explain/$cell_line.$split.o -e logs_explain/$cell_line.$split.e -t 1-0 -p akundaje,euan,owners explain.sh 
+	sbatch -J $cell_line.$split -o logs_explain/$cell_line.$split.o -e logs_explain/$cell_line.$split.e -t 1-0 -p akundaje,euan,owners explain.sh $cell_line $split
     done
 done
