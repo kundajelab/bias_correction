a188fdb1f066459ad504e3e3c4ea3c270241c434
Anna Shcherbina <annashcherbina@gmail.com>
2020-09-02 10:26:30 -0700

    gkmexplain cell lines

--- svm/interpet/explain.sh
+++ svm/interpet/explain.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 task=$1
 split=$2
+chunk=$3
 prefix=/oak/stanford/groups/akundaje/projects/enzymatic_bias_correction/svm/
 model=/oak/stanford/groups/akundaje/projects/enzymatic_bias_correction/svm/models/$task\_dnase/$task.$split.model.txt
-gkmexplain  $prefix/fasta/$task.DNASE.svm.10kb.to.interpret.fa $model  $prefix/pred/gkmexplain.$task.$split.dnase.txt
+gkmexplain  $prefix/fasta/$task.DNASE.svm.10kb.to.interpret.fa.$chunk $model  $prefix/pred/gkmexplain.$task.$split.$chunk.dnase.txt
 
--- svm/interpet/sbatch_explain.sh
+++ svm/interpet/sbatch_explain.sh
@@ -2,6 +2,9 @@ for cell_line in K562 HEPG2
 do
     for split in `seq 0 4`
     do
-	sbatch -J $cell_line.$split -o logs_explain/$cell_line.$split.o -e logs_explain/$cell_line.$split.e -t 1-0 -p akundaje,euan,owners explain.sh $cell_line $split
+	for chunk in `seq 0 9`
+	do
+	sbatch -J $chunk.$cell_line.$split -o logs_explain/$chunk.$cell_line.$split.o -e logs_explain/$chunk.$cell_line.$split.e -t 1-0 -p akundaje,euan,owners explain.sh $cell_line $split $chunk 
+	done
     done
 done
