96c70746851cc46a24df07df0771362de7debdee
annashcherbina <annashcherbina@gmail.com>
2021-06-07 00:55:53 -0700

    Update bpnet_count_tracks.sh

--- enzymatic_bias/bpnet_count_tracks.sh
+++ enzymatic_bias/bpnet_count_tracks.sh
@@ -4,10 +4,10 @@ cur_bam=$1
 
 
 #non-stranded (can be PE or SE) 
-bamCoverage -p16 -v --binSize 1 --samFlagExclude 780 --Offset 1 2 --minMappingQuality 30 -b $cur_bam -o $cur_bam.bpnet.unstranded.bw
+bamCoverage -p16 -v --binSize 1 --samFlagExclude 780 --Offset 1 1 --minMappingQuality 30 -b $cur_bam -o $cur_bam.bpnet.unstranded.bw
 
 #forward strand -- assumes SE data
-bamCoverage -p16 -v --binSize 1 --samFlagExclude 796 --Offset 1 2 --minMappingQuality 30 -b $cur_bam -o $cur_bam.bpnet.plus.bw
+bamCoverage -p16 -v --binSize 1 --samFlagExclude 796 --Offset 1 1 --minMappingQuality 30 -b $cur_bam -o $cur_bam.bpnet.plus.bw
 
 #reverse strand -- assumes SE data
-bamCoverage -p16 -v --binSize 1 --samFlagExclude 780 --samFlagInclude 16 --Offset 1 2 --minMappingQuality 30 -b $cur_bam -o $cur_bam.bpnet.minus.bw
+bamCoverage -p16 -v --binSize 1 --samFlagExclude 780 --samFlagInclude 16 --Offset 1 1 --minMappingQuality 30 -b $cur_bam -o $cur_bam.bpnet.minus.bw
